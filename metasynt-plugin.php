e41f13fc59f52fe73d4e54c6aec66879a1bb868e
Jean Guillaume <jgi@jgwill.com>
2020-06-02 10:11:58 -0400

    chg: hooking after the title

--- metasynt-plugin.php
+++ metasynt-plugin.php
@@ -102,8 +102,8 @@ mes__field_as_markdown_if("questions","","<hr>");
 }
 //---------------------------------------------------------------------------------
 //@STCGoal Feature displaying the Excerpt of the post - Simplify writting an abstract at one place
-add_action('edit_form_after_title','ms__inform__editing_excerpt');
-add_action( 'generate_after_title','mes__b4content' );  
+add_action('edit_form_after_title','ms__inform__editing_excerpt'); //display to editor
+add_action( 'generate_after_title','mes__b4content' );  //display in post
 function mes__b4content()
 {
     echo "<hr>Excerpt: ";
