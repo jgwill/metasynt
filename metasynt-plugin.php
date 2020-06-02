ea2dc91e514b2497841cf6a2a0ad4d89f7a21f2f
Jean Guillaume <jgi@jgwill.com>
2020-06-02 10:32:05 -0400

    style(htmlrender): enhanced display of excerpt in HTML

--- metasynt-plugin.php
+++ metasynt-plugin.php
@@ -105,8 +105,10 @@ mes__field_as_markdown_if("questions","","<hr>");
 add_action('excerpt_edit_pre','ms__inform__editing_excerpt'); //display to editor
 add_action( 'generate_after_entry_title','mes__b4content' );  //display in post
 function mes__b4content()
-{    
-    echo the_excerpt(); //Display the Excerpt
+{   
+    echo "<br><p>
+          ".the_excerpt()."
+            </p>";//Display the Excerpt
 }
 function ms__inform__editing_excerpt()
 {
