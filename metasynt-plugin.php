a79000360444ec7445503a1faeca33faa7fd2307
Jean Guillaume <jgi@jgwill.com>
2020-06-02 10:50:34 -0400

    style(htmlrender): change hook again
    https://docs.generatepress.com/collection/hooks/

--- metasynt-plugin.php
+++ metasynt-plugin.php
@@ -104,7 +104,7 @@ mes__field_as_markdown_if("questions","","<hr>");
 //@STCGoal Feature displaying the Excerpt of the post - Simplify writting an abstract at one place
 add_action('excerpt_edit_pre','ms__inform__editing_excerpt'); //display to editor
 add_action( 'generate_after_page_title','mes__after_title' );  //display in post
-add_action('generate_after_entry_title','mes__b4content');
+add_action('generate_after_entry_header','mes__b4content');
 
 function mes__after_title()
 {
@@ -112,6 +112,7 @@ function mes__after_title()
 }
 function mes__b4content()
 {   
+    echo "<hr>";
    echo "<!-- Except insert Start -->
           ".the_excerpt()."
            <!-- Except insert Ends --> ";//Display the Excerpt
